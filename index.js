24ff0c1ce632230aeac2beae2cdeacd0f53535e5
Joey <joeyhoellerich@gmail.com>
2022-04-09 09:12:29 -0400

    added JS and Comments

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+// create inventory bar
 const inventory = newInventory()
+// place inventory at the bottom left of the screen
 move(inventory).to(0, 0)
 
+// coords for initial character placement
+let x = 100;
+let y = 250;
+
+// create character image
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+// place character at coords
+move(character).to(x, y);
 
+// function used to change the image to look like character is walking
+function handleDirectionChange(direction, element){
+    if (direction == "west"){
+        element.src = "./assets/green-character/west.gif";
+    }
+    if (direction == "east"){
+        element.src = "./assets/green-character/east.gif";
+    }
+    if (direction == "south"){
+        element.src = "./assets/green-character/south.gif";
+    }
+    if (direction == "north"){
+        element.src = "./assets/green-character/north.gif";
+    }
+    if (direction == null){
+        element.src = "./assets/green-character/static.gif";
+    }
+}
 
+// allows player to move character with arrow keys or WASD
+move(character).withKeys(x,y, handleDirectionChange);
+
+// place static images
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
--- move.js
+++ move.js
@@ -1,12 +1,95 @@
 function move(element) {
     element.style.position = 'fixed'
 
+    // function that moves element to set position (static)
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
 
+    // function that moves the element using the arrow or WSDA keys
+    function moveWithKeys(left, bottom, callback) {
+        let direction = null;
+        let x = left;
+        let y = bottom;
+        element.style.zIndex = 1;
+    
+        element.style.left = x + "px";
+        element.style.bottom = y + "px";
+
+        let moveCharacter = () => {
+            // check and make sure that the object is not off the left side of the window
+            if (direction == "west" && x >= 0){
+                x -= 1;
+            }
+
+            // check and make sure that the object is not off the right side of the window
+            if (direction == "east" && x <= window.innerWidth - element.offsetWidth){
+                x += 1;
+            }
+
+            // check and make sure that the object does not go on top of the inventory bar
+            if (direction == "south" && y >= 100){
+                y -= 1;
+            }
+
+            // check and make sure that the object does not go off the top of the window
+            if (direction == "north" && y < window.innerHeight - element.offsetHeight){
+                y += 1;
+            }
+        
+            element.style.left = x + "px";
+            element.style.bottom = y + "px";
+        }
+
+        // move the element every 1 millisecond
+        setInterval(moveCharacter, 1);
+        
+        // check and see if a button is being pressed
+        document.addEventListener("keydown", function(e){
+            // if the same button is being pressed do nothing (return)
+            if (e.repeat) return;
+
+            else if (e.key === "a" || e.key === "A" || e.key === "ArrowLeft"){
+                direction = "west";
+            }
+        
+            else if (e.key === "s" || e.key === "S" || e.key === "ArrowDown"){
+                direction = "south";
+            }
+        
+            else if (e.key === "d" || e.key === "D" || e.key === "ArrowRight"){
+                direction = "east";
+            }
+        
+            else if (e.key === "w" || e.key === "W" || e.key === "ArrowUp"){
+                direction = "north";
+            }
+        
+            else {
+                direction = null;
+            }
+
+            // check to see if callback was defined, if so, run callback
+            if (typeof callback !== "undefined"){
+                callback(direction, element);
+            }
+
+        })
+
+        // when player releases key, stop moving the character (direction == null)
+        document.addEventListener("keyup", function(){
+            direction = null;
+            // check to see if callback was defined, if so, run callback function
+            if (typeof callback !== "undefined"){
+                callback(direction, element);
+            }
+        })
+    }
+
+    // output moveToCoords and moveWithKeys
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withKeys: moveWithKeys
     }
 }
